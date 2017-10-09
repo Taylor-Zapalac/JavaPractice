ed449d53e6c36d2ad403ae37ee3e12ac07df6c4d
Taylor-Zapalac <Taylor.Zapalac@gmail.com>
2017-10-09 15:23:56 -0500

    Note taking on arrays

--- src/DataStructures/Arrays.java
+++ src/DataStructures/Arrays.java
@@ -5,5 +5,40 @@ public class Arrays {
 	public Arrays() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/*
+	 * Practice Array Things
+	 * 
+	 * Create
+	 * Insert
+	 * Capacity
+	 * Size
+	 * Is_Empty
+	 * at
+	 * Prepend
+	 * Push
+	 * Pop
+	 * Remove
+	 * Find
+	 * Resize
+	 * 
+	 * Array: Contiguous area of memory consisting of equal-size elements by contiguous integers.
+	 * Zero-Based indexing
+	 * Constant time access means we can access any point at any time.
+	 * Once initialized, in Java, it cannot be resized.
+	 * Ordered collection of items. Can be any object.
+	 * 
+	 * Why restrict when the size cannot be changed, if lists exist?
+	 * Uses less resources. Most of the time, more restrictions means smaller faster code.
+	 * 
+	 * If we dont know size, list would probably be a better idea.
+	 * 
+	 * 
+	 * 
+	 */
+	public static void Main(String[] args)
+	{
+		
+	}
 
 }
