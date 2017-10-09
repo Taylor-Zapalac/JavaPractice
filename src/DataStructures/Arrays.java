2a37e03249a19266f6e79d23bdaab09f2b0b7a55
Taylor-Zapalac <Taylor.Zapalac@gmail.com>
2017-10-09 18:17:27 -0500

    Arrays

--- src/DataStructures/Arrays.java
+++ src/DataStructures/Arrays.java
@@ -1,5 +1,7 @@
 package DataStructures;
 
+import java.util.Random;
+
 public class Arrays {
 
 	public Arrays() {
@@ -9,18 +11,12 @@ public class Arrays {
 	/*
 	 * Practice Array Things
 	 * 
-	 * Create
-	 * Insert
-	 * Capacity
-	 * Size
-	 * Is_Empty
-	 * at
-	 * Prepend
-	 * Push
-	 * Pop
-	 * Remove
+	 * Create +
+	 * Insert +
+	 * at +
+	 * Remove +
 	 * Find
-	 * Resize
+	 * Resize +
 	 * 
 	 * Array: Contiguous area of memory consisting of equal-size elements by contiguous integers.
 	 * Zero-Based indexing
@@ -59,10 +55,162 @@ public class Arrays {
 	 * I want to implement the 5 main components listed above
 	 * @param args
 	 */
-	public static void Main(String[] args)
+	public static void main(String[] args)
 	{
+		int[] array = generateRandomArray(10);
+		prettyArrayPrint(array);
+		
+		System.out.println("\n");
+		
+		array = insertElement(array, 0, 5);
+		prettyArrayPrint(array);
+		
+		System.out.println("\n");
+		
+		array = resize(array, 11);
+		prettyArrayPrint(array);
+		
+		System.out.println("\n");
+		
+		array = removeAtElement(array, 5);
+		prettyArrayPrint(array);
+		
+		System.out.println("\n");
+		
+		array = add(array, 150);
+		prettyArrayPrint(array);
+		
+		System.out.println("\n");
+		
+		//Sending it a value we already know, because it's generated randomly... slim chance we'll guess whats in there.
+		int foundAt = find(array, 150);
+		if(foundAt >= 0 ) System.out.println(150 + " found at index: "+foundAt);
+		else System.out.println("Not found.");
 		
 	}
+	
+	/**
+	 * Finds passed value within array.
+	 * If isn't found, return -1
+	 * @param array
+	 * @param value
+	 * @return
+	 */
+	public static int find(int[] array, int value)
+	{
+		System.out.println("Finding "+value+":");
+		for(int i = 0; i < array.length; i++)
+		{
+			if(array[i] == value) return i;
+		}
+		return -1;
+	}
+	
+	/**
+	 * Add an element to the end.
+	 * @param array
+	 * @param value
+	 * @return
+	 */
+	public static int[] add(int[] array, int value)
+	{
+		return insertElement(array, value, array.length);
+	}
+	
+	/**
+	 * Remove element at index.
+	 * @param array
+	 * @param index
+	 * @return
+	 */
+	public static int[] removeAtElement(int[] array, int index)
+	{
+		System.out.println("Removing: ");
+		
+		int[] newArray = new int[array.length-1];
+		int position = 0;
+		for(int i = 0; i < array.length; i++)
+		{
+			if(i<index)
+			{
+				newArray[position] = array[i];
+				position++;
+			}
+			else if( i == index)
+			{
+				//Don't insert this element, dont increase position.
+			}
+			else
+			{
+				newArray[position] = array[i];
+				position++;
+			}
+		}
+		
+		return newArray;
+	}
+	
+	/**
+	 * Insert value at element in an array at position, 0-based index
+	 * 
+	 * @param arr
+	 * @param value
+	 * @param position
+	 * @return
+	 */
+	public static int[] insertElement(int[] arr, int value, int position)
+	{
+		System.out.println("Inserting: ");
+		int[] newArray = new int[arr.length+1];
+		
+		
+		for(int i = 0; i <= arr.length; i++)
+		{
+			if(i<position)
+			{
+				//fill from first array
+				newArray[i] = arr[i];
+			}
+			else if( i == position)
+			{
+				//insert value
+				newArray[i] = value;
+			}
+			else
+			{
+				newArray[i] = arr[i-1];
+			}
+		}
+		
+		return newArray;
+	}
+	
+	/**
+	 * Print the array in a nice neat way 
+	 * @param arr
+	 */
+	public static void prettyArrayPrint(int[] arr)
+	{
+		String tablePrint = "";
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			tablePrint += i+"|\t";
+		}
+		System.out.println(tablePrint);
+		
+		tablePrint = "";
+		
+		for(int j = 0; j < arr.length; j++)
+		{
+			tablePrint += arr[j]+"\t";
+		}
+		
+		System.out.println(tablePrint);
+		
+		
+	}
+	
 	
 	/**
 	 * Hard code an array resizer, this assumes the extra slots (if downsizing) are empty. If upsizing, we just make it longer.
@@ -71,10 +219,11 @@ public class Arrays {
 	 * @param newSize size of array we want to return
 	 * @return
 	 */
-	public int[] resize(int[] original, int newSize)
+	public static int[] resize(int[] original, int newSize)
 	{
+		System.out.println("Resize: ");
 		//Get new size, create new array of this size
-		int[] newArray = new int[newSize];
+		int[] newArray = new int[newSize+1];
 		for(int i=0; i <  newSize; i++)
 		{
 			newArray[i] = original[i];
@@ -82,6 +231,25 @@ public class Arrays {
 
 		
 		return newArray;
+	}
+	
+	/**
+	 * Genenerate array of random numbers with specified size.
+	 * Just using this for testing.
+	 * @param size
+	 * @return
+	 */
+	public static int[] generateRandomArray(int size)
+	{
+		System.out.println("Generating: ");
+		int[] randArray = new int[size];
+		Random rand = new Random();
+		for(int i = 0; i < size; i++)
+		{
+			randArray[i] = rand.nextInt(999);
+		}
+		
+		return randArray;
 	}
 
 }
