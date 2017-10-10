ea604ba944887635058fddd68e3d564ffb138bd0
Taylor-Zapalac <Taylor.Zapalac@gmail.com>
2017-10-10 14:20:52 -0500

    Stacks and queue notes

--- src/DataStructures/Que.java
+++ src/DataStructures/Que.java
@@ -6,4 +6,42 @@ public class Que {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	/*
+	 * Queues
+	 * -----
+	 * Kind of the flip side of a Stack
+	 * It is a collection of data that uses First in First out
+	 * FIFO
+	 * 
+	 * Just like a line at the bank or whatever, whoever goes in first, come out first, and everyone shuffles forward
+	 * Takes in order that data is submitted
+	 * 
+	 * In Java queue is an interface, there are a few classes that use this.
+	 * For instance ArrayBlockingQueue and ConcurrentLinked Queue.
+	 * For a simple queue behavior, we would just used the LinkedList class, using add and remove methods.
+	 * 
+	 * Queues are often used in threading.
+	 * 
+	 * Priority Queue
+	 * ---------------
+	 * Some languages allow priority queues
+	 * It allows you to insert new elements with a priority attached, if new element has higher priority than other elements already
+	 * in the queue, it will jump ahead.
+	 * anything lower priority will just line up behind
+	 * Since it's up to you what priority means, you have to provide a comparer
+	 * 
+	 * Deque (Pronounced Deck)
+	 * --------
+	 * Double ended queue
+	 * Like a stack and queue at the same time.
+	 * Can remove and add to both sides.
+	 * Restriction being we cant touch the middles, only the elements at each end.
+	 * In Java LinkedList implements Deque
+	 * 
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
 }
--- src/DataStructures/Stacks.java
+++ src/DataStructures/Stacks.java
@@ -6,4 +6,26 @@ public class Stacks {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	/*
+	 * Stacks
+	 * ------
+	 * Data collections, way to hold multiple items
+	 * Think stack of papers, stack of books, just a stack
+	 * When added to the stack you add to the top
+	 * and when you remove, you take it from the top.
+	 * Last in -> first out
+	 * LIFO Structure
+	 * Do not have numeric index
+	 * All we need is add and remove. that's all we care about
+	 * or Push and Pop
+	 * We can Peek, which shows you the top data, but does not remove like pop
+	 * Very simple because there is not much you can do with it.
+	 * 
+	 * If you want to do anything other than Push, Pop, or Peek you are using the wrong data structure
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
 }
